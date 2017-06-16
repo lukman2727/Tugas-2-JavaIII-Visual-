ba8e4ec07dabc14088491ebe59c91eb4bd6d8035
Lukman Kurniawan <campione47@gmail.com>
2017-06-16 11:14:14 +0700

    Tugas2 Java III visual ComboBox

---
+++ src/tugas2/Tugas2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas2;
+
+import tugas2.frm.identitas;
+
+/**
+ *
+ * @author USER
+ */
+public class Tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        identitas i = new identitas();
+        i.setVisible(true);
+    }
+    
+}
